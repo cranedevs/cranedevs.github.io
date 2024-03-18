35121827b1a9dd2f2d4e285a2215f569536dab1e
Crané <125387403+cranedevs@users.noreply.github.com>
2024-03-18 22:36:07 +0300

    Update logla.js

--- logla.js
+++ logla.js
@@ -1,3 +1,17 @@
-const html = document.documentElement.outerHTML
-const fs = require("fs")
-fs.writeFileSync("anasayf.html", html)
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Page</title>
+  </head>
+  <body>
+    <h1>Hello World</h1>
+  </body>
+</html>
+`
+// save the html file
+const blob = new Blob([html], {type: "text/html"})
+const url = URL.createObjectURL(blob)
+const a = document.createElement("a")
+a.href = url
+a.download = "index.html"
