63d20300bd6926006380448e326bf3a1b02e7f6d
Crané <125387403+cranedevs@users.noreply.github.com>
2024-07-30 23:50:33 +0300

    Update logla.js

--- logla.js
+++ logla.js
@@ -1,15 +1,4 @@
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>My Page</title>
-  </head>
-  <body>
-    <h1>Hello World</h1>
-  </body>
-</html>
-`
-// save the html file
+alert(1);
 const blob = new Blob([html], {type: "text/html"})
 const url = URL.createObjectURL(blob)
 const a = document.createElement("a")
